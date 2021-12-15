81881cabdd3f98d2111274f18b5a452dd09df249
Ahmedbinfaisal <ahmedbinfaisal555@gmail.com>
2021-12-15 17:59:56 +0500

    Update sms-order-plugin.php

--- sms-order-plugin.php
+++ sms-order-plugin.php
@@ -10,11 +10,11 @@
 // Event of a new Email for the order - change in status
 // Event of the order note 
 
-add_action( 'woocommerce_order_status_changed', 'thecodifica_send_sms_on_new_order_status' , 10 ,4);
+//add_action( 'woocommerce_order_status_changed', 'thecodifica_send_sms_on_new_order_status' , 10 ,4);
 add_action( 'woocommerce_order_status_changed', 'thecodifica_send_sms_to_admin' , 10 ,4);
 
 
-add_action( 'woocommerce_new_customer_note_notification' , 'thecodifica_send_sms_on_new_order_note' , 10 , 1);
+//add_action( 'woocommerce_new_customer_note_notification' , 'thecodifica_send_sms_on_new_order_note' , 10 , 1);
 
 
 
@@ -46,43 +46,43 @@ function send_sms_to_customer($phone, $defaultmessage){
 
 // function for sending sms on status changed
 
-function thecodifica_send_sms_on_new_order_status($order_id , $old_status , $new_status , $order){
-    $my_order = wc_get_order($order_id);
-    $orderId = $my_order->get_order_number();
+// function thecodifica_send_sms_on_new_order_status($order_id , $old_status , $new_status , $order){
+//     $my_order = wc_get_order($order_id);
+//     $orderId = $my_order->get_order_number();
 
-    $first_name = $my_order->get_billing_first_name();
+//     $first_name = $my_order->get_billing_first_name();
 
-    $today = date("D M j G:i:s T Y");
-    $phone_number = $my_order->get_billing_phone();
+//     $today = date("D M j G:i:s T Y");
+//     $phone_number = $my_order->get_billing_phone();
 
-    // $shop_name = get_option( 'woocommerce_email_from_name' );
+//     // $shop_name = get_option( 'woocommerce_email_from_name' );
 
-    if($my_order->status == 'processing'){
-        $default_message = "Thank you $first_name for shopping with EZPharmacy. Your order number $orderId is in processing";
-    }elseif($my_order->status == 'on-hold'){
-        $default_message = "Thank you for shopping, $first_name. Your order status has been set to on-hold. We'll get back to you shortly";
-    }elseif($my_order->status == 'completed'){
-        $default_message = "Thank you for shopping with us $first_name. Your Order $orderId has been set as completed. Make sure to give your review on our website! https://phar.ezshifa.com/";
-    }elseif($my_order->status == 'shipped'){
-        $default_message = "Thank you for shopping with us $first_name. Your order has been picked up by on $today . Your track code is ";
-    }
+//     if($my_order->status == 'processing'){
+//         $default_message = "Thank you $first_name for shopping with EZPharmacy. Your order number $orderId is in processing";
+//     }elseif($my_order->status == 'on-hold'){
+//         $default_message = "Thank you for shopping, $first_name. Your order status has been set to on-hold. We'll get back to you shortly";
+//     }elseif($my_order->status == 'completed'){
+//         $default_message = "Thank you for shopping with us $first_name. Your Order $orderId has been set as completed. Make sure to give your review on our website! https://phar.ezshifa.com/";
+//     }elseif($my_order->status == 'shipped'){
+//         $default_message = "Thank you for shopping with us $first_name. Your order has been picked up by on $today . Your track code is ";
+//     }
 
 
-    send_sms_to_customer($phone_number , $default_message );
+//     send_sms_to_customer($phone_number , $default_message );
 
-}
+// }
 
 // fucntion for sending sms on new order note
 
-function thecodifica_send_sms_on_new_order_note($email_args){
+// function thecodifica_send_sms_on_new_order_note($email_args){
 
-    $order = wc_get_order($email_args['order_id']);
-    $note = $email_args['customer_note'];
+//     $order = wc_get_order($email_args['order_id']);
+//     $note = $email_args['customer_note'];
 
-    $phone = $order->get_billing_phone();
+//     $phone = $order->get_billing_phone();
 
-    send_sms_to_customer($phone , $note);
-}
+//     send_sms_to_customer($phone , $note);
+// }
 
 function thecodifica_send_sms_to_admin($order_id , $old_status , $new_status , $order){
     $my_order = wc_get_order($order_id);
